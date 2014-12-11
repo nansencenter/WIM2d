05049c8ca874814a1ac71a40eb5725b3aaad1535
tdcwilliams <tdcwilliams@gmail.com>
2014-12-11 18:51:30 +0100

    non-io code called with mex
    TODO - io version of code

---
+++ fortran/run/run_WIM2d.m
@@ -0,0 +1,44 @@
+%% /Users/twilliams/GITHUB-REPOSITORIES/WIM2d/fortran/run/run_WIM2d.m
+%% Author: Timothy Williams
+%% Date:   20141211, 17:49:41 CET
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% make directories in which to put outputs:
+dirs  = {'out',
+         'out/log',
+         'out/binaries',
+         'out/binaries/prog'};
+
+for j=1:length(dirs)
+   dd = dirs{j};
+   if ~exist(dd)
+      mkdir(dd);
+   end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 1
+   %% run non-io 2d WIM:
+   disp('Running 2d WIM (on in/out)...');
+   disp(' ');
+   addpath('../bin');
+   WIM2d_run_mex;
+   %%
+   !cat out/log/wim2d.log
+   %%
+   disp(' ');
+   disp('Finished running 2d WIM.');
+   disp(' ');
+   disp('Outputs in:');
+   disp(dirs);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if 1
+   %% test:
+   cd test;
+   startup_local;
+   test_WIM2d_F;
+   cd ..;
+end
