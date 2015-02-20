a87915b20fa07b53530cf808b4b0ca4a45355e68
tdcwilliams <tdcwilliams@gmail.com>
2015-02-20 16:56:23 +0100

    function to load grid

---
+++ fortran/matlab_funs/fn_get_grid.m
@@ -0,0 +1,55 @@
+function grid_prams  = fn_get_grid(outdir)
+
+afile    = [outdir,'/binaries/wim_grid.a'];
+bfile    = [outdir,'/binaries/wim_grid.b'];
+afile2   = [outdir,'/binaries/wim_init.a'];
+
+grid_prams  = struct('nx'        ,[],...
+                     'ny'        ,[],...
+                     'dx'        ,[],...
+                     'dy'        ,[],...
+                     'X'         ,[],...
+                     'Y'         ,[],...
+                     'scuy'      ,[],...
+                     'scvx'      ,[],...
+                     'scp2'      ,[],...
+                     'scp2i'     ,[],...
+                     'LANDMASK'  ,[]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% get basic info from bfile
+bid   = fopen(bfile);
+C     = textscan(bid,'%2.2d %s %s %s',1);
+nrec  = C{1};
+%%
+C              = textscan(bid,'%3.3d %s %s %s %s %s %s',1);
+grid_prams.nx  = C{1};
+%%
+C              = textscan(bid,'%3.3d %s %s %s %s %s %s',1);
+grid_prams.ny  = C{1};
+fclose(bid);
+
+nx = grid_prams.nx;
+ny = grid_prams.ny;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%read afile
+fmt   = 'float32';
+aid   = fopen(afile);
+%%
+grid_prams.X         = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.Y         = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.scuy      = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.scvx      = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.scp2      = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.scp2i     = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+grid_prams.LANDMASK  = reshape( fread(aid,nx*ny,fmt) ,nx,ny );
+%%
+fclose(aid);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+grid_prams.dx  = mean(grid_prams.scvx(:));
+grid_prams.dy  = mean(grid_prams.scuy(:))
+
+return;
