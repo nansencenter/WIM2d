f89a44ba149c274f7059da9a8611f00c205309a1
tdcwilliams <tdcwilliams@gmail.com>
2015-03-27 11:43:40 +0100

    add examples of header files

---
+++ fortran/header_files/examples/grid_info.h
@@ -0,0 +1,9 @@
+      !! header file with grid dimensions and resolution
+      !! - file generated automatically with setup_grid.py/save_grid_f2py.F
+      !! - do not edit !!
+
+      integer,parameter   :: ii = 150   !! Record length in x direction (elements)
+      integer,parameter   :: jj = 002   !! Record length in y direction (elements)
+ 
+      real,parameter   :: dx = 0.4000E+04   !! Resolution in x direction
+      real,parameter   :: dy = 0.4000E+05   !! Resolution in y direction
