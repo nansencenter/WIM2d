189d4e86bd2543025b33b651056c957c8221ba5f
tdcwilliams <tdcwilliams@gmail.com>
2015-03-31 22:10:02 +0200

    bug in fns_boltzmann_steady

--- fortran/run/fig_scripts/fig_test_convergence2steady.py
+++ fortran/run/fig_scripts/fig_test_convergence2steady.py
@@ -15,7 +15,7 @@ sys.path.append(dd+"/py_funs")
 
 # interface to fortran code
 # - compile with single frequency
-import run_WIM2d     as Rwim 
+import run_WIM2d     as Rwim
 
 # other python modules
 import WIM2d_f2py    as Mwim
@@ -57,7 +57,7 @@ if 1:
    ########################################################################
    c_in     = .7     # conc
    h_in     = 2.     # thickness
-   D_in     = 100.   # initial floe size 
+   D_in     = 100.   # initial floe size
    Hs_in    = 3.     # initial Hs
    Tp_in    = 12.    # initial Tp
    mwd_in   = -90    # initial mean wave direction
@@ -85,7 +85,7 @@ if 1:
    # change real parameters:
    young          = 5.0e9
    visc_rp        = 0.0
-   duration_hours = 12.0
+   duration_hours = 48.0
    duration       = duration_hours*60*60
    real_prams     = np.array([young,visc_rp,duration])
 
@@ -97,7 +97,11 @@ out_fields,outdir = Rwim.do_run(RUN_OPT=RUN_OPT,in_fields=in_fields,
 ##########################################################################
 # Make plots
 bindir   = outdir+'/binaries'
-figdir   = 'fig_scripts/figs/TC2S'
+figdir0  = 'fig_scripts/figs/'
+figdir   = figdir0+'TC2S'
+
+if not os.path.exists(figdir0):
+   os.mkdir(figdir0)
 if not os.path.exists(figdir):
    os.mkdir(figdir)
 
@@ -123,30 +127,41 @@ if 1:
    out   = Fbs.solve_boltzmann_ft(width=ice_width,
             alp=alp,N=N,alp_dis=alp_dis,cg=cg,f_inc=Fbs.dirspec_inc_spreading,Hs=Hs_in)
 
-   nx0         = 200
-   x_ice       = np.linspace(0.,200.,nx0)
+   # alp2  = out['inputs'][0]
+   # print(alp,alp2)
+   # sys.exit('fig_test_convergence2steady')
+
+   nx0         = 1.e3
+   x_ice       = np.linspace(0.,ice_width,nx0)
    E_n         = Fbs.calc_energy(out,x_ice,L=ice_width,n_test=[0])
    Hs_steady   = 4*np.sqrt(E_n[:,0].real)
 
-   fig      = Fplt.plot_1d(1.e-3*(xe+x_ice),Hs_steady,labs=labs,color='m')
+   # print(x_ice)
+   # print(Hs_steady)
+   fig      = Fplt.plot_1d(1.e-3*(xe+x_ice),Hs_steady,labs=labs,color='y',linewidth=3)
 else:
    fig   = None
 ##########################################################################
 
+steps2plot  = range(0,600,40)
+# steps2plot  = range(0,140,40)
+cols     = ['k','b','r','g','m','c']
+lstil    = ['-','--','-.',':']
+Nc       = len(cols)
+loop_c   = -1
+loop_s   = 0
 
-# steps2plot  = range(0,300,40)
-steps2plot  = range(0,140,40)
-cols        = ['k','b','r','g',
-               'k','b','r','g',
-               'k','b','r','g']
-
-loop_i   = -1
 for nstep in steps2plot:
    out_fields  = Fdat.fn_check_prog(outdir,nstep) # load ice/wave conditions from binaries
    Hs_n        = out_fields['Hs']
    #
-   loop_i   = loop_i+1
-   fig      = Fplt.plot_1d(xx,Hs_n,labs=labs,f=fig,color=cols[loop_i])
+   if loop_c==Nc-1:
+      loop_c   = 0
+      loop_s   = loop_s+1
+   else:
+      loop_c   = loop_c+1
+
+   fig      = Fplt.plot_1d(xx,Hs_n,labs=labs,f=fig,color=cols[loop_c],linestyle=lstil[loop_s])
 #
 figname  = figdir+'/convergence2steady.png'
 print('saving to '+figname+'...')
@@ -154,51 +169,52 @@ plt.savefig(figname,bbox_inches='tight',pad_inches=0.05)
 plt.close()
 fig.clf()
 
-sys.exit('fig_test_convergence2steady.py, L94')
+# sys.exit('fig_test_convergence2steady.py, L94')
 ##########################################################################
 
-figdir1  = figdir+'/init/'
-Fplt.fn_plot_init(grid_prams,ice_fields,wave_fields,figdir1) # plot initial conditions
-print("Plots in "+figdir+"/init")
-print(" ")
-##########################################################################
-
-################################################################
-# Look at end results:
-print("Plotting results...")
-figdir2  = figdir+'/final/'
-Fplt.fn_plot_final(grid_prams,out_fields,figdir2)
-print("Plots in "+figdir2+'\n')
-print(" ")
-################################################################
-
-if 1:
-   ################################################################
-   # Plot progress files (if they exist)
-   figdir3     = figdir+'/prog'
-   prog_files  = os.listdir(bindir+'/prog')
-   steps       = []
-   for pf in prog_files:
-      if '.a'==pf[-2:]:
-         stepno   = pf[-5:-2]
-         steps.append(stepno)
-
-   # make dir for progress plots
-   if (not os.path.exists(figdir3)) and len(steps)>0:
-      os.mkdir(figdir3)
-
-   # clear old progress plots
-   old_dirs = os.listdir(figdir3)
-   for od in old_dirs:
-      # need this for macs
-      if od!='.DS_Store':
-         # os.rmdir(figdir3+'/'+od)
-         shutil.rmtree(figdir3+'/'+od)
-
-   for stepno in steps:
-      print("Plotting results at time step "+stepno+" ...")
-      prog_fields = Fdat.fn_check_prog(outdir,int(stepno))
-      figdir3_0   = figdir3+'/'+stepno
-      Fplt.fn_plot_final(grid_prams,prog_fields,figdir3_0)
-      print("Plots in "+figdir3_0+'\n')
-   ################################################################
+# figdir1                 = figdir+'/init/'
+# ice_fields,wave_fields  = Fdat.fn_check_init(bindir)
+# Fplt.fn_plot_init(grid_prams,ice_fields,wave_fields,figdir1) # plot initial conditions
+# print("Plots in "+figdir+"/init")
+# print(" ")
+# ##########################################################################
+# 
+# ################################################################
+# # Look at end results:
+# print("Plotting results...")
+# figdir2  = figdir+'/final/'
+# Fplt.fn_plot_final(grid_prams,out_fields,figdir2)
+# print("Plots in "+figdir2+'\n')
+# print(" ")
+# ################################################################
+# 
+# if 1:
+#    ################################################################
+#    # Plot progress files (if they exist)
+#    figdir3     = figdir+'/prog'
+#    prog_files  = os.listdir(bindir+'/prog')
+#    steps       = []
+#    for pf in prog_files:
+#       if '.a'==pf[-2:]:
+#          stepno   = pf[-5:-2]
+#          steps.append(stepno)
+# 
+#    # make dir for progress plots
+#    if (not os.path.exists(figdir3)) and len(steps)>0:
+#       os.mkdir(figdir3)
+# 
+#    # clear old progress plots
+#    old_dirs = os.listdir(figdir3)
+#    for od in old_dirs:
+#       # need this for macs
+#       if od!='.DS_Store':
+#          # os.rmdir(figdir3+'/'+od)
+#          shutil.rmtree(figdir3+'/'+od)
+# 
+#    for stepno in steps:
+#       print("Plotting results at time step "+stepno+" ...")
+#       prog_fields = Fdat.fn_check_prog(outdir,int(stepno))
+#       figdir3_0   = figdir3+'/'+stepno
+#       Fplt.fn_plot_final(grid_prams,prog_fields,figdir3_0)
+#       print("Plots in "+figdir3_0+'\n')
+#    ################################################################
