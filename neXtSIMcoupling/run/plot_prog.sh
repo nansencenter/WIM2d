ecc033fef52894912b6cf4f61fcd08b33319446f
tdcwilliams <tdcwilliams@gmail.com>
2015-04-17 16:28:04 +0200

    scripts to make test plots/movies
    from 'prog' files

---
+++ neXtSIMcoupling/run/plot_prog.sh
@@ -0,0 +1,18 @@
+# make png figures from progress files
+# (and of initial and final, if wanted)
+python plot_prog.py
+
+MKMOV=$1 # 1, make movies; 0, don't
+if [ $MKMOV -eq 1 ]
+then
+   # make movies of these variables:
+   vbl_list="Hs Dmax taux tauy"
+   cd test_outputs
+
+   tools="${WIM2D_PATH}/fortran/tools"
+
+   for vbl in $vbl_list
+   do
+      $tools/prog2mp4.sh $vbl
+   done
+fi
