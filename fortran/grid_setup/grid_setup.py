81fb2fae1a3d22d5b917472c9302242c9427ed4e
tdcwilliams <tdcwilliams@gmail.com>
2015-04-29 13:48:01 +0200

    infile for grid_setup.py
    no need to change script if want different grid

--- fortran/grid_setup/grid_setup.py
+++ fortran/grid_setup/grid_setup.py
@@ -7,13 +7,23 @@ import fns_grid_setup as gs
 CHANGE_GRID    = 1
 CHANGE_WAVES   = 1
 
-# Set grid configuration (see fns_grid_setup.py)
+# Set default grid configuration (see fns_grid_setup.py)
 if 0:
    GRID_OPT = 0 # standard 1d grid configuration
 elif 1:
    GRID_OPT = 2 # Philipp's small-square
 else:
    GRID_OPT = 1 # standard 2d grid configuration
+
+#############################################################
+infile   = 'infile_grid.txt'
+if os.path.exists(infile):
+   # read in GRID_OPT from file:
+   fid      = open(infile,'r')
+   lines    = fid.readlines()
+   fid.close()
+   GRID_OPT = int(lines[0].split()[0])
+#############################################################
 
 TEST     = 0 # if 0: save grid files to ../run/inputs (correct place for rest of model to find)
              # if 1: save grid files to test/out_py (for testing: rest of model can't find outputs)
