d390b2ccafb4cb5b44a269947ee05cf19120fcdf
tdcwilliams <tdcwilliams@gmail.com>
2015-05-18 07:37:12 +0200

    function to calculate energy from eigen_info
    tested in fig_Boltzmann_Steady.m

--- matlab/boltzmann/fig_Boltzmann_Steady.m
+++ matlab/boltzmann/fig_Boltzmann_Steady.m
@@ -14,24 +14,86 @@ absorb   = 0;
 lam0  = freq;
 ISO   = alp_scat;
 
-out = fn_Boltzmann_Steady(fortyp, lam0, conc, wth, ISO, absorb);
+RIGID = 5.49;%GPa - Young's modulus
+Param = ParamDef_Default(RIGID);
+%%
+Ni          = 1;
+Vert_Modes  = 200;
+Nangles     = 100;
+Param = ModParam_def(Param,1,Vert_Modes,0,0,Nangles);
+%%
+COMM  = 1;
+
+%PLOT  = 0;%% No plotting inside fn_Boltzmann_Steady.m
+PLOT  = 2;%% Plot inside
+
+outputs  = 'eigen-info';
+
+figure(1);
+clf;
+out = fn_Boltzmann_Steady(fortyp, lam0, conc, wth, ISO, absorb,...
+                          Param,outputs,COMM,PLOT);
+nplots   = 0;
+if PLOT==2
+   legend_labs = {'SSB (LB)'};
+   nplots      = 1;
+end
+
 for j=1:length(out)
    disp(out(j).name)
    disp(out(j).value)
+   if ~isempty(strfind(out(j).name,'eigen-info'))
+      eigen_info  = out(j).value;
+   end
+end
+
+if 1
+   nplots   = nplots+1;
+   Hs_inc   = 3;
+   x_edge   = -220e3;
+   xvec_ss  = x_edge+linspace(0,wth,400).';
+   Ndir     = length(eigen_info.angles);%%now different to Nangles - why?
+   %%
+   [Edir,j_inc]   = fn_Boltzmann_calcEdir(eigen_info,xvec_ss-xvec_ss(1),Hs_inc);
+   dtheta         = 2*pi/Ndir;
+   m0             = dtheta*sum(Edir,2);
+   Hs_ss          = real(4*sqrt(m0));
+
+   if 0
+      %%test normalisation of Edir
+      m0_inc   = dtheta*sum(Edir(:,incs),2);
+      [m0_inc(1),m0(1)]
+      [4*sqrt(m0_inc(1)),Hs_ss(1)]
+      return
+   end
+
+   legend_labs{end+1}   = 'SSB (LB2)';
+   figure(1);%%ratio
+   hold on;
+   plot((xvec_ss-xvec_ss(1))/1e3,Hs_ss/Hs_ss(1),'--r');
+   %%
+   figure(2);%%absolute value of Hs (m)
+   plot(xvec_ss/1e3,Hs_ss,'k');
+   xl = xlabel('x, km');
+   yl = ylabel('H_s, m');
+   set(xl,'fontname','Times','fontsize',14);
+   set(yl,'fontname','Times','fontsize',14);
 end
 
 %% add plots made by python/fortran code
-figdir         = '/Users/timill/GITHUB-REPOSITORIES/WIM2d/fortran/run/fig_scripts/figs/TC2S';
-file_name{1}   = [figdir,'/test_steady1.dat'];%%time-dependant code after a long time
-file_name{2}   = [figdir,'/test_steady2.dat'];%%steady state
-file_name{3}   = [figdir,'/test_steady2_FT.dat'];%%steady state (FT)
-cols           = {'-b','--r','--c'};
-legend_labs    = {'SSB (LB)','SSB (TW1)','SSB (TW2)','Time-dep'};
+w2d_path       = getenv('WIM2D_PATH');
+figdir         = [w2d_path,'/fortran/run/fig_scripts/figs/TC2S'];
+file_name{1}   = [figdir,'/test_steady2.dat'];%%steady state
+file_name{2}   = [figdir,'/test_steady2_FT.dat'];%%steady state (FT)
+file_name{3}   = [figdir,'/test_steady1.dat'];%%time-dependant code after a long time
+cols           = {'--c','--m','-b'};
+leg_text       = {'SSB (TW1)','SSB (TW2)','Time-dep'};
 
-nplots   = 1;
-for j=3:-1:1
-   fname = file_name{j};
+DO_INIT  = 1;
+for j=1:3
+   fname = file_name{j}
    if exist(fname)
+      legend_labs{end+1}   = leg_text{j};
       nplots   = nplots+1;
       fid      = fopen(fname);
 
@@ -56,17 +118,18 @@ for j=3:-1:1
 
       x  = C{1};
       Hs = C{2};
-      if nplots==2
-         x0 = x(1);
+      if DO_INIT
+         x0       = x(1);
+         DO_INIT  = 0;
       end
       jz = find(x>=x0,1,'first');
       y0 = Hs(jz);
       %%
       figure(1);
       hold on;
-      plot((x-x0),Hs/y0,cols{j});
+      plot((x-x0)/1e3,Hs/y0,cols{j});
       hold off;
-      xlim([0,wth]);
+      xlim([0,wth/1e3]);
       %%
       figure(2);
       hold on;
@@ -94,7 +157,11 @@ saveas(gcf,figname);
 if nplots>1
    figure(2);
    box on;
-   legend(legend_labs{2:nplots});
+   if PLOT==2
+      legend(legend_labs{2:nplots});
+   else
+      legend(legend_labs{1:nplots});
+   end
    figname  = 'out/fig_eg_HsVsX_abs.png';
    disp(['Saving to ',figname]);
    saveas(gcf,figname);
---
+++ matlab/boltzmann/fn_Boltzmann_calcEdir.m
@@ -0,0 +1,70 @@
+function [I,incs] = fn_Boltzmann_calcEdir(eigen_info,x,Hs_inc)
+%% CALL: I  = fn_Boltzmann_calcEdir(eigen_info,x,Hs_inc)
+%% Inputs:
+%% eigen_info   = struct('u0',NaN,...
+%%                       'v0',NaN,...
+%%                       'V',V,...
+%%                       'D0',D0,...
+%%                       'D1',D1,...
+%%                       'coeffs',c,...
+%%                       'width',wth,...
+%%                       'absorb',absorb,...
+%%                       'angles_on_pi',th_vec);
+%% - structure calculated by fn_Boltzmann_Steady.m;
+%% x is vector of positions to calculate I at (default: [0,width]);
+%% Hs_inc is significant wave height of incident waves (default: 1m);
+%%
+%% Outputs:
+%% I: rows are directional energy spectrum at each point of x
+
+u0       = eigen_info.u0;
+v0       = eigen_info.v0;
+V        = eigen_info.V;
+D0       = eigen_info.D0;
+D1       = eigen_info.D1;
+c        = eigen_info.coeffs;
+wth      = eigen_info.width;
+th_vec   = eigen_info.angles/pi;
+absorb   = eigen_info.absorb;
+
+if ~exist('x','var')
+   x  = [0;wth];
+end
+
+if ~exist('Hs_inc','var')
+   Hs_inc   = 1;
+end
+
+refs  = find(or(th_vec>0.5,th_vec<-0.5)); %% indices of reflected waves
+incs  = find(~or(th_vec>0.5,th_vec<-0.5));%% indices of incident  waves
+Ndir  = length(th_vec);
+Nx    = length(x);
+I     = zeros(Nx,Ndir);
+
+if absorb==0
+   %% x  = 0
+   I0 = [u0,V]*diag([1;1;exp(D0*0);exp(D1*(0-wth))])*c;
+   for loop_x=1:length(x)
+      Ix = [x(loop_x)*v0+u0,V]*diag([1;1;exp(D0*x(loop_x));exp(D1*(x(loop_x)-wth))])*c;
+      %%
+      I(loop_x,:) = real(Ix).';
+   end
+else
+   %% x  = 0
+   I0 = V*diag([exp(D0*0);exp(D1*(0-wth))])*c;
+   for loop_x=1:length(x)
+      Ix = V*diag([exp(D0*x(loop_x));exp(D1*(x(loop_x)-wth))])*c;
+      %%
+      I(loop_x,:) = real(Ix).';
+   end
+end
+
+dtheta   = 2*pi/Ndir;
+m0_inc   = sum(I0(incs))*dtheta;          %% integrate wrt theta over incident directions
+I        = (.25*Hs_inc)^2/real(m0_inc)*I; %% renormalise I so inc waves have required Hs at x=0
+
+if 0
+   dtheta
+   m0_p  = sum(I(1,incs),2)*dtheta
+   Hs_p  = 4*sqrt(m0_p)
+end
