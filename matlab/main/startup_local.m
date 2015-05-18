001c89fe97499f491cd20bccfedbb6cca4bd55e0
tdcwilliams <tdcwilliams@gmail.com>
2015-05-18 11:17:06 +0200

    startup files

--- matlab/main/startup_local.m
+++ matlab/main/startup_local.m
@@ -17,13 +17,18 @@ issmdir  = getenv('ISSM_DIR');
 spath    = path;
 jissm    = strfind(path,issmdir);
 for j=1:length(jissm)
+
    path2 = spath(jissm(j):end);
    jcol  = strfind(path2,':');
    if length(jcol)>0
       path2 = path2(1:jcol(1)-1);
    end
+
+   disp(' ');
+   disp(['rmpath ',path2]);
    rmpath(path2)
 end
+disp(' ');
 %gitdir   = '~/GITHUB-REPOSITORIES/';
 %feval(fxn,[gitdir,'matlab/Semi-Infinite-Elastic-Plate/ND_progs']);
 %feval(fxn,[gitdir,'matlab/Semi-Infinite-Elastic-Plate/GEN_progs']);
