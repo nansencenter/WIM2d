0c8e78ffe6c6b16875dc4824e4096097d15c8fa8
tdcwilliams <tdcwilliams@gmail.com>
2015-06-25 14:27:16 +0200

    1d plot prog option

--- fortran/tools/plot_prog.py
+++ fortran/tools/plot_prog.py
@@ -3,10 +3,10 @@ import os
 import sys
 import shutil
 import struct
-# import matplotlib.rcsetup as rc
+import matplotlib.pyplot as plt
 
-wim2d_path=os.getenv('WIM2D_PATH')
-dd   = os.path.abspath(wim2d_path+"/fortran")
+wim2d_path  = os.getenv('WIM2D_PATH')
+dd          = os.path.abspath(wim2d_path+"/fortran")
 sys.path.append(dd+"/bin")
 sys.path.append(dd+"/py_funs")
 
@@ -25,8 +25,13 @@ grid_prams  = Fdat.fn_check_grid(bindir)
 if not os.path.exists(figdir):
    os.mkdir(figdir)
 
+PLOT_INIT   = 0
+PLOT_FINAL  = 0
+PLOT_PROG   = 1
+PROG_OPT    = 1
+
 ##########################################################################
-if 1:
+if PLOT_INIT:
    # Look at initial fields:
    print("Plotting initial conditions...")
    ice_fields,wave_fields  = Fdat.fn_check_init(bindir) # load initial conditions from binaries
@@ -38,7 +43,7 @@ if 1:
 ##########################################################################
 
 ################################################################
-if 1:
+if PLOT_FINAL:
    # Look at end results:
    print("Plotting final results...")
    figdir2     = figdir+'/final/'
@@ -48,7 +53,7 @@ if 1:
    print(" ")
 ################################################################
 
-if 1:
+if PLOT_PROG:
    ################################################################
    # Plot progress files (if they exist)
    prog_files  = os.listdir(bindir+'/prog')
@@ -62,7 +67,7 @@ if 1:
    Nprogs   = len(steps)
    print(str(Nprogs)+' sets of progress files to plot\n')
 
-   if 1:
+   if PROG_OPT==1:
       # plot as colormaps
       figdir3     = figdir+'/prog'
 
@@ -93,19 +98,95 @@ if 1:
       print('**********************************************************************\n')
       ################################################################
 
-   elif 1:
+   elif PROG_OPT==2:
+
       ################################################################
-      # Plot as profiles
-      steps2plot  = range(0,600,40)
-      # steps2plot  = range(0,140,40)
+      # Plot as profiles on different graphs (eg test advection)
+      cols     = ['k']
+      lstil    = ['-']
+
+      figdir3     = figdir+'/prog'
+      first_call  = True
+      xx          = grid_prams['X'][:,0]
+      labs        = ['$x$, km','$H_s$, m']
+
+      # make dir for progress plots
+      if (not os.path.exists(figdir3)) and len(steps)>0:
+         os.mkdir(figdir3)
+
+      for stepno in steps:
+         print("Plotting results at time step "+stepno+" ...")
+         prog_fields = Fdat.fn_check_prog(outdir,stepno)
+         figdir3_0   = figdir3+'/'+stepno
+
+         Hs_n  = prog_fields['Hs'][:,0]
+         if first_call:
+            Hs_0        = prog_fields['Hs'][:,0]
+            fig         = None
+            first_call  = False
+
+         fig   = Fplt.plot_1d(xx/1.e3,Hs_n,labs=labs,f=fig,color='k',linestyle='-')
+         if 0:
+            # TODO: calc & plot "Hs_exact" from speed in x direction
+
+            ################################################
+            # set manually
+            T        = 12. # wave period (s)
+            wavdir0  = -90.# waves-from dirn (degrees)
+            CFL      = 0.7
+            ################################################
+
+            ################################################
+            dir0     = -np.pi/180.*(90.+wavdir0)   # waves-to dirn (radians)
+            om       = 2*np.pi/T
+            g        = 9.81
+            k        = om*om/g
+            cp       = om/k
+            cg       = cp/2.
+            ug       = cg*np.cos(dir0)
+            #
+            dx    = xx[1]-xx[0]
+            dt    = CFL*dx/cg
+            xt    = xx+ug*int(stepno)*dt
+            fig   = Fplt.plot_1d(xt/1.e3,Hs_0,labs=labs,f=fig,color='r',linestyle='--')
+            ################################################
+
+         plt.xlim([xx.min()/1.e3,xx.max()/1.e3])
+         plt.ylim([0.,2.*Hs_0.max()])
+
+         if not os.path.exists(figdir3_0):
+            os.mkdir(figdir3_0)
+         figname  = figdir3_0+'/Hs.png'
+         print('saving to '+figname+'...\n')
+         plt.savefig(figname,bbox_inches='tight',pad_inches=0.05)
+         plt.close()
+         fig.clf()
+
+      print('**********************************************************************')
+      print('to make movie, go to figs/prog and type')
+      print(wim2d_path+'/fortran/tools/prog2mp4.sh Hs')
+      print('or')
+      print(wim2d_path+'/fortran/tools/prog2mp4.sh Dmax')
+      print('**********************************************************************\n')
+      ################################################################
+
+   elif PROG_OPT==3:
+      ################################################################
+      # Plot as profiles on same graph (test convergence to steady)
+      print(steps)
+      steps2plot  = range(len(steps))
+      #
       cols     = ['k','b','r','g','m','c']
       lstil    = ['-','--','-.',':']
       Nc       = len(cols)
       loop_c   = -1
       loop_s   = 0
+      xx       = grid_prams['X'][:,0]
+      fig      = None
+      labs     = ['$x$, km','$H_s$, m']
 
       for nstep in steps2plot:
-         out_fields  = Fdat.fn_check_prog(outdir,nstep) # load ice/wave conditions from binaries
+         out_fields  = Fdat.fn_check_prog(outdir,steps[nstep]) # load ice/wave conditions from binaries
          Hs_n        = out_fields['Hs']
          #
          if loop_c==Nc-1:
