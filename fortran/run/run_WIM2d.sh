2887fe1513577f367299e11cb4c5bbc8834ab0a7
Jon E. Bergh <joribe@geneva.ad.nersc.no>
2016-05-11 09:38:59 +0200

    run fortran code; infile templates

--- fortran/run/run_WIM2d.sh
+++ fortran/run/run_WIM2d.sh
@@ -1,5 +1,27 @@
 # place to put results
-outdir="out"
+
+if [ ! -f infile_dirs.txt ]
+then
+   echo "infile_dirs.txt not present, copy template from infiles"
+   exit
+else
+   lst=(`cat infile_dirs.txt`)
+   indir=${lst[0]}
+   outdir=${lst[1]}
+   if [ ! -f $indir/wim_grid.a ];
+   then
+      echo "$indir/wim_grid.a missing";
+      echo "Please go to ../grid_setup to set up grid"
+      exit
+   fi
+   if [ ! -f $indir/wim_grid.b ];
+   then
+      echo "$indir/wim_grid.b missing";
+      echo "Please go to ../grid_setup to set up grid"
+      exit
+   fi
+fi
+
 mkdir -p $outdir
 mkdir -p $outdir/log
 mkdir -p $outdir/binaries
@@ -14,9 +36,9 @@ $w2d/fortran/bin/WIM2d.exec
 if [ $# -eq 0 ]
 then
    echo To make plots of progress:
-   echo $w2d/fortran/tools/plot_prog.sh 0 out
+   echo $w2d/fortran/tools/plot_prog.sh 0 $outdir
    echo To make plots of progress, and make a movie:
-   echo $w2d/fortran/tools/plot_prog.sh 1 out
+   echo $w2d/fortran/tools/plot_prog.sh 1 $outdir
    echo
    echo Else, enter an argument 0 or 1 with this script ie
    echo "./run_WIM2d.sh 0 (plots, no movie)"
@@ -28,5 +50,5 @@ else
 fi
 
 # make plots + movie
-echo $w2d/fortran/tools/plot_prog.sh $MK_MOV out
-$w2d/fortran/tools/plot_prog.sh $MK_MOV out
+echo $w2d/fortran/tools/plot_prog.sh $MK_MOV $outdir
+$w2d/fortran/tools/plot_prog.sh $MK_MOV $outdir
