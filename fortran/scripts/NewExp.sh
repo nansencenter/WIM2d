755592bb4e8416a1dac7d28b335626cc7dab0556
tdcwilliams <tdcwilliams@gmail.com>
2016-07-27 12:20:47 +0200

    fortran setup script; output log file

--- fortran/scripts/NewExp.sh
+++ fortran/scripts/NewExp.sh
@@ -3,38 +3,72 @@
 # make a new area to run fortran WIM from
 GS=$WIM2D_PATH/fortran/grid_setup
 BUILD=$WIM2D_PATH/fortran/Build
+TOOLS=$WIM2D_PATH/fortran/tools
 
-# check infiles for grid
-if [ ! -f "infile_grid.txt" ] || [ ! -f "infile_waves.txt" ]
-then
-   echo "Missing either infiles_grid.txt or infiles_waves.txt"
-   echo "- Get templates from $GS/infiles"
-   exit
-fi
+bdir=Build_F
+tdir=scripts
 
-mkdir -p Build_F
-mv infile_grid.txt infile_waves.txt Build_F
-cd Build_F
+# ========================================================
+P=`pwd`
+Bdir=$P/$bdir
+mkdir -p $Bdir
 
-# set up grid
-$GS/scripts/grid_setup.sh
+cd $Bdir
+ln -s $BUILD/Makefile Makefile
+cp $GS/infiles/grid/infile_grid.txt $GS/infiles/waves/infile_waves.txt .
 
-# get Makefile
-ln -s $BUILD/Makefile .
-cd ..
-echo "Compile in Build_F"
-echo "Run here with code from $WIM2D_PATH/fortran/scripts"
+cd $P
+echo " "
+echo "*******************************************************"
+echo "To set up grid and compile:"
+echo "cd $bdir;"
+echo "Edit infiles_grid.txt & infiles_waves.txt;"
+echo "Type ../$tdir/grid_setup.sh"
+echo "Compile with 'make' (pure fortran),"
+echo "'make mex1', 'make mex2' or 'make mex3' (mex functions),"
+echo "or 'make py' (f2py),"
+echo "*******************************************************"
+echo " "
+# ========================================================
 
+# ========================================================
+# link useful scripts here
+Tdir=$P/$tdir
+mkdir -p $Tdir
+
+# script to set-up grid
+ln -s $GS/scripts/grid_setup.sh $Tdir/grid_setup.sh
+
+# plotting tools
+for f in $TOOLS/*
+do
+   g=`basename $f`
+   ln -s $f $Tdir/$g
+done
+# ========================================================
+
+
+# ========================================================
 # for pure fortran
+cd $P
 cp $WIM2D_PATH/fortran/scripts/infiles/infile_nonIO.txt .
-echo "For pure fortran,"
+ln -s $WIM2D_PATH/fortran/scripts/run_WIM2d.sh
+
+
+echo " "
+echo "*******************************************************"
+echo "To run pure fortran,"
 echo "edit options in infile_nonIO.txt"
 echo "and run with"
-echo "$WIM2D_PATH/fortran/scripts/run_WIM2D.sh"
+echo "$tdir/run_WIM2D.sh"
+echo " "
 
 # to enable matlab (mex functions)
-cp $WIM2D_PATH/fortran/scripts/startup_local.m .
+ln -s $WIM2D_PATH/fortran/scripts/startup_local.m .
 cp $WIM2D_PATH/matlab/main/infiles/infile_matlab.txt .
 
 echo "To use mex functions, edit infile_matlab.txt"
-echo "and type run('$WIM2D_PATH/matlab/run_WIM2d.m');"
+echo "and (in matlab) type run('$WIM2D_PATH/matlab/run_WIM2d.m');"
+echo "for pure matlab set MEX_OPT=0."
+echo "*******************************************************"
+echo " "
