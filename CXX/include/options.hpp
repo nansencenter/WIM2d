8cf2b95cffdd806f92b99d8b52a525a6e3d7248b
Abdoulaye <abdoulaye.samake@nersc.no>
2016-08-09 11:49:30 +0200

    Add options.hpp

---
+++ CXX/include/options.hpp
@@ -0,0 +1,22 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*- */
+
+/**
+ * @file   options.hpp
+ * @author Abdoulaye Samake <abdama@beijing.ad.nersc.no>
+ * @date   Fri Aug  5 16:07:13 2016
+ */
+
+#ifndef __WIMOPTIONS_H
+#define __WIMOPTIONS_H 1
+
+#include "boost/program_options.hpp"
+
+namespace Wim
+{
+namespace po = boost::program_options;
+
+po::options_description
+descrWimOptions();
+} // WIMOPT
+
+#endif
