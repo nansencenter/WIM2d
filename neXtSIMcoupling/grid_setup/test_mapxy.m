ae382dc5710dcc24fac7af30591d5cf6cca4ae50
tdcwilliams <tdcwilliams@gmail.com>
2016-11-11 13:26:35 +0100

    code for testing mapxy.m and mapll.m
    agrees with pyproj exactly for same parameters

--- neXtSIMcoupling/grid_setup/test_mapxy.m
+++ neXtSIMcoupling/grid_setup/test_mapxy.m
@@ -1,7 +1,7 @@
 x  = (-20:20)';%km
 y  = 12;%km
 
-TEST_MPROJ  = 1; %% 1: test m_xy2ll from m_proj; 0: test projection used in nextsim c++ code
+TEST_MPROJ  = 0; %% 1: test m_xy2ll from m_proj; 0: test projection used in nextsim c++ code
 if TEST_MPROJ
    %% m_xy2ll,m_ll2wy use sphere of radius 1
    %% stereographic projection has true scale lat at center of projection
@@ -15,9 +15,13 @@ for i=1:length(x)
    xi = x(i);
    if TEST_MPROJ==1
       [lon,lat]   = m_xy2ll(xi/R,y/R);
+      [X,Y]       = m_ll2xy(lon,lat);
+      X           = R*X;
+      Y           = R*Y;
    else
       [lat,lon]   = mapxy(xi,y,60,-45,'N');
+      [X,Y]       = mapll(lat,lon,60,-45,'N');
    end
-   ss          = sprintf('%f   %f   %f   %f',xi,y,lon,lat);
+   ss = sprintf('%f   %f   %f   %f   %f   %f',xi,y,X,Y,lon,lat);
    disp(ss);
 end
