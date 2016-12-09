47af6bc2147afc935a6f4036a451be6a5a9277f4
tdcwilliams <tdcwilliams@gmail.com>
2016-12-09 23:35:40 +0100

    Pierson-Moskowitz spectrum
    also calculates wind speed, Tp and Hs, given any one of these

---
+++ matlab/other_deps/SDF_Pierson_Moskowitz.m
@@ -0,0 +1,59 @@
+%% SDF_Pierson_Moskowitz.m
+%% Author: Timothy Williams
+%% Date: 20161209
+
+function [params,S]  = SDF_Pierson_Moskowitz(params,omega,moment)
+
+if ~exist('params','var')
+   params.U10  = 10;
+end
+if ~exist('moment','var')
+   moment   = 0;
+end
+
+%% 1st calc parameters
+alp      = 8.1e-3;
+bet      = .74;
+g        = 9.81;
+if isfield(params,'Hs')
+   U_19pt5     = (g^2*params.Hs^2*bet/(4*alp))^.25;
+   params.U10  = U_19pt5/1.026;
+   om_0        = g/U_19pt5;
+   params.Tp   = 2*pi/(.877*om_0);%%peak period
+elseif isfield(params,'U10')
+   U_19pt5     = 1.026*params.U10;
+   params.Hs   = 2/g*U_19pt5^2*sqrt(alp/bet);
+   om_0        = g/U_19pt5;
+   params.Tp   = 2*pi/(.877*om_0);%%peak period
+elseif isfield(params,'Tp')
+   om_0        = 2*pi/params.Tp/.877;
+   U_19pt5     = g/om_0;
+   params.U10  = U_19pt5/1.026;
+   params.Hs   = 2/g*U_19pt5^2*sqrt(alp/bet);
+end
+
+%% only calc spectrum if needed
+if nargout==1
+   return%comment to do testing
+end
+
+DO_TEST  = 0;
+if ~exist('omega','var')
+   DO_TEST  = 1;
+   T        = (25:-1:1)';
+   omega    = 2*pi./T;
+   return;
+end
+
+f1 = alp*g^2*omega.^(-5);
+f2 = exp(-bet*(om_0./omega).^4)
+S  = f1.*f2;
+
+if DO_TEST
+   params
+   S_B   = SDF_Bretschneider(omega,{params.Tp,params.Hs,moment});
+   plot(2*pi./omega,S);
+   hold on;
+   plot(2*pi./omega,S_B,'--r');
+   hold off;
+end
