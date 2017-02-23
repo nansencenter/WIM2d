9c05161096fe6fd22f02e06a037f1de00785fba0
tdcwilliams <tdcwilliams@gmail.com>
2017-02-23 21:24:46 +0100

    changes to test function for ice advection code

--- matlab/numerics/testing/test_iceadv_weno.m
+++ matlab/numerics/testing/test_iceadv_weno.m
@@ -2,15 +2,16 @@
 %% Author: Timothy Williams
 %% Date:   20170217
 clear;
+format long;
 
-MEX      = 1;
+MEX      = 1
 CFL      = .7;
 DO_FIG2  = 1;%2x2 plot: landmask,u,v,h
 
 %%testing:
 if 0
-   %OPT   = 1;
-   OPT   = 2;
+   OPT   = 1;
+   %OPT   = 2;
    ii = 150;
    jj = 10;
    iland = 80:90;
@@ -56,17 +57,18 @@ grid_prams  = s1;
 clear s1;
 
 
-nbdy  = 4;
-masks = make_masks_puv(1-grid_prams.LANDMASK,nbdy);
+nbdy     = 4;
+masks    = make_masks_puv(1-grid_prams.LANDMASK,nbdy);
+masks_i  = make_masks_puv(1-grid_prams.LANDMASK,nbdy,'rows');
 
 if OPT==1
    uc      = 30;%const speed m/s
    xc      = 2*xm/3;
    %theta   = 170;%wave-to direction [deg] - to left & up
-   theta   = 180;%wave-to direction [deg] - to left
+   %theta   = 180;%wave-to direction [deg] - to left
    %theta   = 135;%wave-to direction [deg] - up and to the left
    %theta   = 0;%wave-to direction [deg] - to right
-   %theta   = 45;%wave-to direction [deg] - up and to the right
+   theta   = 45;%wave-to direction [deg] - up and to the right
    %theta    = 90;%up
    u       = 0*X+uc*cos(pi/180*theta);
    v       = 0*X+uc*sin(pi/180*theta);
@@ -95,7 +97,7 @@ elseif OPT==3
    jwave    = find((R<Rm)&(X>0)&(abs(Y)>Ym));%%rhs of pacman
    h(jwave) = 1;
    %%
-   fac      = -.5;
+   fac      = -1.5;
    angrot   = fac*(1/20)*pi/180;%%radian/s
    u        = 0*X;
    v        = 0*X;
@@ -159,16 +161,6 @@ if DO_FIG2%%plot u,v,h
    subplot(2,2,3);
    Z  = 0*X;
    Z(1:end-1,1:end-1)   = h;
-   %[C,H] = contour(X,Y,Z,[1,1]);
-   [C,H] = contour(X,Y,Z,[0,0]);
-   nc    = C(2,1);
-   x1    = C(1,(1:nc)+1);
-   y1    = C(2,(1:nc)+1);
-   %%
-   [C,H] = contour(X,Y,Z,[1,1]);
-   nc    = C(2,1);
-   x2    = C(1,(1:nc)+1);
-   y2    = C(2,(1:nc)+1);
    %%
    cla;
    ax = pcolor(X/1e3,Y/1e3,Z);
@@ -179,13 +171,6 @@ if DO_FIG2%%plot u,v,h
    ttl   = title('h(0), m');
    GEN_font(ttl);
    GEN_proc_fig('x, km','y, km');
-   %%
-   if OPT==3
-      hold on;
-      plot(x1/1e3,y1/1e3,'r');
-      plot(x2/1e3,y2/1e3,'m');
-      hold off;
-   end
    %%
    subplot(2,2,4);
    Z  = 0*X;
@@ -205,12 +190,6 @@ if DO_FIG2%%plot u,v,h
    GEN_font(ttl);
    GEN_proc_fig('x, km','y, km');
    %%
-   if OPT==3
-      hold on;
-      plot(x1/1e3,y1/1e3,'r');
-      plot(x2/1e3,y2/1e3,'m');
-      hold off;
-   end
    GEN_pause;
 end
 
@@ -218,16 +197,28 @@ h0 = h;
 for n = 1:nt
    [n,nt]
    if MEX==0
+      'iceadv_weno'
       h           = iceadv_weno(h,u,v,grid_prams,masks,dt,nbdy);
       Diag.hmax   = max(h(:));
       Diag.mass   = sum(h(:))
    else
-      %[h,test]  =...
-      h  =...
-            iceadv_weno_mex(grid_prams.nx,grid_prams.ny,dt,nbdy,h, u, v,...
-               grid_prams.scp2, grid_prams.scp2i, grid_prams.scuy, grid_prams.scvx,...
-               masks.pmask,masks.umask,masks.vmask,masks.isp,masks.isu,masks.isv,...
-               masks.ifp,masks.ifu,masks.ifv,masks.ilp,masks.ilu,masks.ilv);
+      if MEX==1
+         'iceadv_weno_mex'
+         %[h,test]    =...
+         h  =...
+               iceadv_weno_mex(grid_prams.nx,grid_prams.ny,dt,nbdy,h, u, v,...
+                  grid_prams.scp2, grid_prams.scp2i, grid_prams.scuy, grid_prams.scvx,...
+                  masks_i.pmask,masks_i.umask,masks_i.vmask,masks_i.isp,masks_i.isu,masks_i.isv,...
+                  masks_i.ifp,masks_i.ifu,masks_i.ifv,masks_i.ilp,masks_i.ilu,masks_i.ilv);
+      else
+         'iceadv_weno_matmem_mex'
+         %[h,test]  =...
+         h  =...
+               iceadv_weno_matmem_mex(grid_prams.nx,grid_prams.ny,dt,nbdy,h, u, v,...
+                  grid_prams.scp2, grid_prams.scp2i, grid_prams.scuy, grid_prams.scvx,...
+                  masks.pmask,masks.umask,masks.vmask,masks.isp,masks.isu,masks.isv,...
+                  masks.ifp,masks.ifu,masks.ifv,masks.ilp,masks.ilu,masks.ilv);
+      end
 
       Diag.hmax   = max(h(:));
       Diag.mass   = sum(h(:))
@@ -235,7 +226,24 @@ for n = 1:nt
          test
          return
       elseif ~DO_FIG2
-         [h1,test1]  = iceadv_weno(h0,u,v,grid_prams,masks,dt,nbdy);
+         if 1
+            'compare to matlab function'
+            [h1,test1]  = iceadv_weno(h0,u,v,grid_prams,masks,dt,nbdy);
+         elseif MEX==1
+            'compare to original mex function (matmem)'
+            h1 =...
+                  iceadv_weno_matmem_mex(grid_prams.nx,grid_prams.ny,dt,nbdy,h0, u, v,...
+                     grid_prams.scp2, grid_prams.scp2i, grid_prams.scuy, grid_prams.scvx,...
+                     masks.pmask,masks.umask,masks.vmask,masks.isp,masks.isu,masks.isv,...
+                     masks.ifp,masks.ifu,masks.ifv,masks.ilp,masks.ilu,masks.ilv);
+         else
+            'compare to new mex function'
+            h1 =...
+                  iceadv_weno_mex(grid_prams.nx,grid_prams.ny,dt,nbdy,h0, u, v,...
+                     grid_prams.scp2, grid_prams.scp2i, grid_prams.scuy, grid_prams.scvx,...
+                     masks_i.pmask,masks_i.umask,masks_i.vmask,masks_i.isp,masks_i.isu,masks_i.isv,...
+                     masks_i.ifp,masks_i.ifu,masks_i.ifv,masks_i.ilp,masks_i.ilu,masks_i.ilv);
+         end
          if 1
             PADDED   = 0;
             tst      = h1-h;
@@ -349,35 +357,6 @@ for n = 1:nt
       ttl   = title('h(t), m');
       GEN_font(ttl);
       GEN_proc_fig('x, km','y, km');
-
-      if OPT==1
-         x1 = xc+uc*cos(pi*theta/180)*n*dt;
-         y1 = -ym+uc*sin(pi*theta/180)*n*dt;
-         x2 = xm+uc*cos(pi*theta/180)*n*dt;
-         hold on;
-         plot(x1/1e3+0*yy(yy>y1),yy(yy>y1)/1e3,'r');
-         plot(x2/1e3+0*yy(yy>y1),yy(yy>y1)/1e3,'r');
-         plot([x1,x2]/1e3,y1/1e3*[1 1],'r');
-         if 1
-            y3 = 0*xx;
-            y3((xx>=x1)&(xx<=x2))   = ym/2;
-            plot(xx/1e3,y3/1e3,'k');
-            plot(xx/1e3,h(:,end)*ym/2/1e3,'--k');
-            yb = ym/2+0*xx;
-            plot(xx/1e3,yb/1e3,'--r');
-         end
-         hold off;
-
-      elseif OPT==3
-         hold on;
-         x3 = x1*cos(n*dtheta)-y1*sin(n*dtheta);
-         y3 = y1*cos(n*dtheta)+x1*sin(n*dtheta);
-         plot(x3/1e3,y3/1e3,'r');
-         x4 = x2*cos(n*dtheta)-y2*sin(n*dtheta);
-         y4 = y2*cos(n*dtheta)+x2*sin(n*dtheta);
-         plot(x4/1e3,y4/1e3,'m');
-         hold off;
-      end
    end
    %GEN_pause;
    %pause(.1);
