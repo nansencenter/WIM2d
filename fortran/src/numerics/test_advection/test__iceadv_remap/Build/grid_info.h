d4f542623065ee75a6461bbba9de0f96f94963db
tdcwilliams <tdcwilliams@gmail.com>
2017-03-17 14:21:29 +0100

    rename directory

--- fortran/src/numerics/test_advection/test__iceadv_remap/Build/grid_info.h
+++
@@ -1,9 +0,0 @@
-      !! header file with grid dimensions and resolution
-      !! - file generated automatically in fortran/grid_setup
-      !! - do not edit !!
-
-      integer,parameter   :: ii = 0150   !! Record length in x direction (elements)
-      integer,parameter   :: jj = 0010   !! Record length in y direction (elements)
- 
-      real,parameter   :: dx = 0.4000E+04   !! Resolution in x direction
-      real,parameter   :: dy = 0.4000E+04   !! Resolution in y direction
