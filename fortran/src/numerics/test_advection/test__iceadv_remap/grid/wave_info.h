714a425089cbf8508e4f3cdc42edbcadfeee9b47
tdcwilliams <tdcwilliams@gmail.com>
2017-03-17 14:15:36 +0100

    reorganise some directories in test_advection

---
+++ fortran/src/numerics/test_advection/test__iceadv_remap/grid/wave_info.h
@@ -0,0 +1,5 @@
+      integer,parameter   :: n_wavdir    = 0016
+      integer,parameter   :: n_wave_freq = 0001 !should be odd (Simpson's rule)
+
+      real,parameter   :: Tmin =   2.50
+      real,parameter   :: Tmax =  25.00
