2b0c415db80bd7dc14708ba6a2bf6385f8018afc
tdcwilliams <tdcwilliams@gmail.com>
2018-05-03 11:01:59 +0200

    pep8

--- fortran/grid_setup/scripts/grid_plot.py
+++ fortran/grid_setup/scripts/grid_plot.py
@@ -1,14 +1,14 @@
 import os,sys
 import matplotlib.pyplot as plt
 
-w2d   = os.getenv('WIM2D_PATH')
+w2d = os.getenv('WIM2D_PATH')
 sys.path.append(w2d+'/fortran/py_funs')
 import fns_get_data as Fdat
 import fns_plot_data as Fplt
 
-gdir     = '.'
-gf       = Fdat.fn_check_grid(gdir)
-fields   = {'LANDMASK':gf['LANDMASK']}
+gdir   = '.'
+gf     = Fdat.fn_check_grid(gdir)
+fields = {'LANDMASK':gf['LANDMASK']}
 Fplt.fn_plot_gen(gf,fields,'out/')
 
 figname = 'land_mask.png'
