eabb6b42c9f8f97698e425a6981c373c41b7776c
tdcwilliams <tdcwilliams@gmail.com>
2018-05-03 11:24:19 +0200

    tidy up

--- neXtSIMcoupling/tutorial/mohr_coulomb.py
+++ neXtSIMcoupling/tutorial/mohr_coulomb.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-tau0  = 40.e3  # 
-mu    = .7     # 
+tau0 = 40.e3  # 
+mu   = .7     # 
 
 tau_t = 42.e3  # max sigN
 nu    = .3
 alp   = (1+nu)/(1-nu)
 
-sigN  = np.arange(-100.,60.,1.)*1.e3
-taup  = tau0-mu*sigN
-taum  = -tau0+mu*sigN
+sigN = np.arange(-100.,60.,1.)*1.e3
+taup = tau0-mu*sigN
+taum = -tau0+mu*sigN
 
 tauw  = alp*sigN
 sigNp = tau0/(alp+mu)
@@ -18,9 +18,9 @@ sigNm = -tau0/(alp-mu)
 sigC  = np.array([sigNp,sigNm])
 tauC  = alp*sigC
 
-sig11_p     = 2./(1-nu)*sigNp
-epsc_coeff  = (1-nu*nu)*sig11_p
-Yvec        = 1.e9*np.arange(.5,10.1,.1)
+sig11_p    = 2./(1-nu)*sigNp
+epsc_coeff = (1-nu*nu)*sig11_p
+Yvec       = 1.e9*np.arange(.5,10.1,.1)
 
 f  = plt.figure()
 
