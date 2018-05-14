8d9f36cef179b6a088831844b8d001b174caf50b
tdcwilliams <tdcwilliams@gmail.com>
2018-05-14 12:37:51 +0200

    fig_Boltzmann_Steady: - now a function - make MIZ width a function input - comments - plot names depend on MIZ width

--- matlab/boltzmann/fig_Boltzmann_Steady.m
+++ matlab/boltzmann/fig_Boltzmann_Steady.m
@@ -2,30 +2,44 @@
 %% script to get, plot results from fn_Boltzmann_Steady.m (from Luke Bennetts)
 %% Author: Timothy Williams
 %% Date: 20150507
+function fig_Boltzmann_steady(wth_km)
+
+if ~exist('wth_km','var')
+   wth  = 100e3;%m
+   wstr = num2str(wth/1e3);% for fig names
+elseif wth_km=='inf'
+   wth  = 'inf';
+   wstr = 'inf';% for fig names
+else
+   wth  = wth_km*1e3;%m
+   wstr = num2str(wth_km);% for fig names
+end
 
 period   = 12;
 fortyp   = 'freq';
 freq     = 1/period;
 conc     = 1;
-wth      = 100e3;%m
 alp_scat = 1.42798715E-04; %m^{-1}
 absorb   = 0;
 %%
-lam0  = freq;
-ISO   = alp_scat;
+lam0 = freq;
+% ISO  = 0; %% use full elastic plate calculation
+ISO  = alp_scat;%%isotropic scattering
 
 RIGID = 5.49;%GPa - Young's modulus
 Param = ParamDef_Default(RIGID);
 %%
-Ni          = 1;
-Vert_Modes  = 200;
-Nangles     = 100;
-Param = ModParam_def(Param,1,Vert_Modes,0,0,Nangles);
+Ni         = 1;
+Vert_Modes = 200;
+Nangles    = 100;
+Param = ModParam_def(Param,1,Vert_Modes,0,0,Nangles)
+   %% NB Param.MIZ_length is only used for plotting
+   %% - wth is the thing used for the calculations
 %%
 COMM  = 1;
 
 %PLOT  = 0;%% No plotting inside fn_Boltzmann_Steady.m
-PLOT  = 2;%% Plot inside
+PLOT  = 2;%% Plot inside - and compare to external energy calculation
 
 outputs  = 'eigen-info';
 
@@ -33,31 +47,35 @@ figure(1);
 clf;
 out = fn_Boltzmann_Steady(fortyp, lam0, conc, wth, ISO, absorb,...
                           Param,outputs,COMM,PLOT);
-nplots   = 0;
+legend_labs = {};
+col_v2 = '-k';
 if PLOT==2
-   legend_labs = {'SSB (LB)'};
-   nplots      = 1;
+   legend_labs{end+1} = 'SSB (v1)';
+   col_v2 = '--r';
 end
 
 for j=1:length(out)
    disp(out(j).name)
    disp(out(j).value)
    if ~isempty(strfind(out(j).name,'eigen-info'))
-      eigen_info  = out(j).value;
+      eigen_info = out(j).value;
    end
 end
 
 if 1
-   nplots   = nplots+1;
-   Hs_inc   = 3;
-   x_edge   = -220e3;
-   xvec_ss  = x_edge+linspace(0,wth,400).';
-   Ndir     = length(eigen_info.angles);%%now different to Nangles - why?
+   Hs_inc  = 3;
+   x_edge  = -220e3;
+   if wth == 'inf'
+      xvec_ss = x_edge+linspace(0,200e3,400).';
+   else
+      xvec_ss = x_edge+linspace(0,wth,400).';
+   end
+   Ndir    = length(eigen_info.angles);%%now different to Nangles - why?
    %%
-   [Edir,j_inc]   = fn_Boltzmann_calcEdir(eigen_info,xvec_ss-xvec_ss(1),Hs_inc);
-   dtheta         = 2*pi/Ndir;
-   m0             = dtheta*sum(Edir,2);
-   Hs_ss          = real(4*sqrt(m0));
+   [Edir,j_inc] = fn_Boltzmann_calcEdir(eigen_info,xvec_ss-xvec_ss(1),Hs_inc);
+   dtheta       = 2*pi/Ndir;
+   m0           = dtheta*sum(Edir,2);
+   Hs_ss        = real(4*sqrt(m0));
 
    if 0
       %%test normalisation of Edir
@@ -67,20 +85,29 @@ if 1
       return
    end
 
-   legend_labs{end+1}   = 'SSB (LB2)';
+   legend_labs{end+1} = 'SSB (v2)';
+   xp_km = (xvec_ss-xvec_ss(1))/1e3;
    figure(1);%%ratio
    hold on;
-   plot((xvec_ss-xvec_ss(1))/1e3,Hs_ss/Hs_ss(1),'--r');
+   plot(xp_km,Hs_ss/Hs_ss(1),col_v2);
+   xl = xlabel('x, km');
+   yl = ylabel('H_s/H_{s,inc}');
+   set(xl,'fontname','Times','fontsize',14);
+   set(yl,'fontname','Times','fontsize',14);
    %%
    figure(2);%%absolute value of Hs (m)
-   plot(xvec_ss/1e3,Hs_ss,'k');
+   plot(xp_km,Hs_ss,'k');
    xl = xlabel('x, km');
    yl = ylabel('H_s, m');
    set(xl,'fontname','Times','fontsize',14);
    set(yl,'fontname','Times','fontsize',14);
 
    %% print to dat-file:
-   dfile_mat   = 'out/test_steady_mat.dat';
+   outdir = 'out';
+   if ~exist(outdir,'dir')
+      mkdir(outdir)
+   end
+   dfile_mat   = [outdir,'/test_steady_mat.dat'];
    disp(' ');
    disp('Saving steady-state results from fn_Boltzmann_[Steady/calcEdir].m to:');
    disp(dfile_mat);
@@ -108,13 +135,13 @@ file_name{3}   = [figdir,'/test_steady1.dat'];%%time-dependant code after a long
 cols           = {'--c','--m','-b'};
 leg_text       = {'SSB (TW1)','SSB (TW2)','Time-dep'};
 
-DO_INIT  = 1;
+DO_INIT = 1;
+jleg = max(1,length(legend_labs));
 for j=1:3
    fname = file_name{j}
    if exist(fname)
-      legend_labs{end+1}   = leg_text{j};
-      nplots   = nplots+1;
-      fid      = fopen(fname);
+      legend_labs{end+1} = leg_text{j};
+      fid = fopen(fname);
 
       %% read past header
       found_hashes   = 0;
@@ -158,30 +185,24 @@ for j=1:3
       set(xl,'fontname','Times','fontsize',14);
       set(yl,'fontname','Times','fontsize',14);
       hold off;
+   else
+      disp(['file ', fname, ' not present - skipping']);
    end
 end
 
 %% add legends and save fig's
-if ~exist('out','dir')
-   !mkdir out
-end
-
 figure(1);
 box on;
-legend(legend_labs{1:nplots});
-figname  = 'out/fig_eg_HsVsX_ratio.png';
+if length(legend_labs)>0
+   legend(legend_labs);
+end
+figname  = [outdir,'/fig_eg_HsVsX_ratio_W',wstr,'.png'];
 disp(['Saving to ',figname]);
 saveas(gcf,figname);
 
-if nplots>1
-   figure(2);
-   box on;
-   if PLOT==2
-      legend(legend_labs{2:nplots});
-   else
-      legend(legend_labs{1:nplots});
-   end
-   figname  = 'out/fig_eg_HsVsX_abs.png';
-   disp(['Saving to ',figname]);
-   saveas(gcf,figname);
-end
+figure(2);
+box on;
+legend(legend_labs{jleg:end});
+figname  = [outdir,'/fig_eg_HsVsX_abs_W',wstr,'.png'];
+disp(['Saving to ',figname]);
+saveas(gcf,figname);
